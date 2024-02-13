9c73e2adce4523b2d1db7926be47aaf37533b2f2
kimjunwoo <junwoo1027@naver.com>
2024-02-14 02:27:53 +0900

    중첩 클래스를 다루는 방법

---
+++ src/com/kotlin/lec13/Lec13.kt
@@ -0,0 +1,11 @@
+package com.kotlin.lec13
+
+class House(
+    private val address: String,
+    private val livingRoom: LivingRoom
+) {
+    inner class LivingRoom(private val area: Double) {
+        val address: String
+            get() = this@House.address
+    }
+}
